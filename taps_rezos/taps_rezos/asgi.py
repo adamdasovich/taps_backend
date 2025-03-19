66be12202095a481fa1e6acbce5a192348f3c8d0
Adam Dasovich <adamdasovich@gmail.com>
2025-03-19 09:49:23 -0400

    mm

--- taps_rezos/taps_rezos/asgi.py
+++ taps_rezos/taps_rezos/asgi.py
@@ -11,7 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module ='taps_backend.deployment' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'taps_rezos.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+# settings_module ='taps_backend.deployment' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'taps_rezos.settings'
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taps_backend.settings')
 
 application = get_asgi_application()
