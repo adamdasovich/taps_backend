8c505ade2c9c79b62601235febbe2c7fedcc16b8
Adam Dasovich <adamdasovich@gmail.com>
2025-03-28 08:10:01 -0400

    fixed the wsgi.py

--- taps_rezos/wsgi.py
+++ taps_rezos/wsgi.py
@@ -8,6 +8,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taps_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taps_rezos.settings')
 
 application = get_wsgi_application()
